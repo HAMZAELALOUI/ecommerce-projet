4883de4a531aeca32741a513b4cfaf91d9f013b1
SKITINA <97435283+SKITINA@users.noreply.github.com>
2025-06-25 10:35:27 +0100

    Deploy to Heroku

--- ecommercebackend/config/db.js
+++ ecommercebackend/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Configuration de la connexion à la base de données MySQL
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ecommerce'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'ecommerce'
 });
 
 // Connexion à la base de données
